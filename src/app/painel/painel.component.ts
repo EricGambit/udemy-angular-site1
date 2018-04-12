8c53a0b5ca7e8c2ebd3bca3c02593e250e4a8819
Eric Gambit <ericlopespedroso@gmail.com>
2018-04-11 21:52:35 -0300

    Criado fluxo de atualização da barra de progresso ao avaliar a resposta

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,24 +12,48 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = "Traduza a frase"
   public frases: Frase[] = FRASES
-  public resposta: string
+  public resposta: string = ''
 
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progressoPai: number = 0
 
   constructor() {
-    console.log(this.frases)
+    this.atualizaRodada();
   }
 
   ngOnInit() {
   }
 
   public atualizaResposta(resposta: Event): void {
-    this.resposta = (<HTMLInputElement>resposta.target).value
-
-    //console.log(this.resposta)
+    this.resposta = (<HTMLInputElement>resposta.target).value   
   }
 
-  public verificarResposta():void{
-    console.log("Verificar resposta: " + this.resposta )
+  public verificarResposta(): void {
+
+    if (this.resposta == this.rodadaFrase.frasePtBr) {
+      //Trocar pergunta da rodada
+      this.rodada++
+
+      //Progresso da barra
+      this.progressoPai = this.progressoPai + (100 / this.frases.length)
+
+      //Atualiza objeto da rodadaFrase
+      this.atualizaRodada();
+
+      //limpar a resposta
+      this.resposta = ''
+
+    } else {
+
+    }
+
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase);
   }
 
 }
